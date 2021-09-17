4a396888d8916c3ee67b9d369ed4dec209ff0d94
LongChu2003 <long.chu@inbox.ru>
2021-09-17 20:56:31 +0300

    P(3.1)

---
+++ homeworks/3/P(3.1).java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class P(3.1) {
+    public static void main(String[] args) {
+        Scanner vvod = new Scanner(System.in);
+        int n = vvod.nextInt();
+        //boolean isProst = true;
+        int countProst = 0;
+
+        for (int i = 2; i <= 100000; i++)
+        {
+            boolean isProst = true;
+            for (int j = 2; j < i; j++)
+            {
+                //System.out.println("i = "+ i);
+                //System.out.println("j = "+ j);
+                if (i % j == 0)
+                {
+                    isProst = false;
+                }
+            }
+            if (isProst)
+            {
+                System.out.print(i + " ");
+                countProst++;
+            }
+            if (countProst == n)
+                break;
+
+        }
+        System.out.println("");
+        System.out.println("кол-во простых чисел: "+ countProst);
+    }
+}
