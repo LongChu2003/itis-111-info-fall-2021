e507d6c6083aa9f789ea58abe0e2599d3946e250
Mansur Ziiatdinov <gltronred@gmail.com>
2021-10-07 12:51:17 +0300

    CW1

---
+++ cw1/1/C1W1.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+public class C1W1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // Решение задачи 1 следует писать здесь
+        System.out.println("Solve me!");
+    }
+}
