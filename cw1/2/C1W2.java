305c4143776c87d7c124b6a0ab48fd99ba86a1e2
LongChu2003 <long.chu@inbox.ru>
2021-10-07 14:55:09 +0300

    C1W2

--- cw1/2/C1W2.java
+++ cw1/2/C1W2.java
@@ -1,10 +1,30 @@
-import java.io.*;
-import java.util.*;
-
+import java.util.Scanner;
 public class C1W2 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        // Решение задачи 2 следует писать здесь
-        System.out.println("Solve me!");
+    public static void main(String[] args){
+        Scanner vvod = new Scanner(System.in);
+        int n = vvod.nextInt();
+        int count_Chet = 0;
+        int chet_do= 0;
+        int max = n;
+        while (n!=-1)
+        {
+            if (n % 2 ==0)
+            {
+                count_Chet++;
+                System.out.println("count_Chet = " + count_Chet);
+
+            }
+            if (max < n)
+            {
+                max = n;
+                System.out.println("max = "+max);
+                chet_do = count_Chet;
+                System.out.println("chet_do = " + chet_do);
+                count_Chet = 0;
+                System.out.println("count_Chet = " + count_Chet);
+            }
+            n = vvod.nextInt();
+        }
+        System.out.println(max + "  " +chet_do);
     }
 }
