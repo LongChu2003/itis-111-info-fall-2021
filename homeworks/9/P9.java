b5fd61d7af450fcea31316fd7924f71baae2687b
LongChu2003 <long.chu@inbox.ru>
2021-10-19 21:52:54 +0300

    P9

--- homeworks/9/P9.java
+++ homeworks/9/P9.java
@@ -5,25 +5,77 @@ public class P9 {
         int n = vvod.nextInt();
         int m =vvod.nextInt();
         int[][] a = new int [n][m];
-        int s = 1;
+        int s = 1; //счетчик, который заполняет значение массива
+
+        //двигаемся влево
         for (int j = 0;j < m; j++){
             a[0][j] = s;
             s++;
         }
+
+        //двигаемся вниз
         for (int i = 1; i< n; i++){
             a[i][m-1] = s;
             s++;
         }
+
+        //двигаемся вправо
         for (int j = m-2; j>=0; j-- ){
             a[n-1][j] = s;
             s++;
         }
+
+        //двигаемся вверх
         for (int i =n-2;i>0;i--){
             a[i][0] = s;
             s++;
         }
+// заполнили внешний прямоугольник
+
+
+        int x = 1;//координаты ячейки a[x][y]
+        int y = 1;//координаты ячейки a[x][y]
+
+        while (s < n*m) {
+            
+            //двигаемся вправо
+            while (a[x][y + 1] == 0) {
+                a[x][y] = s;
+                s++;
+                y++;
+            }
+
+            //двигаемся вниз
+            while (a[x + 1][y] == 0) {
+                a[x][y] = s;
+                s++;
+                x++;
+            }
+
+            //двигаемся влево
+            while (a[x][y-1] == 0) {
+                a[x][y] = s;
+                s++;
+                y--;
+            }
+
+            //двигаемся вверх
+            while (a[x - 1][y] == 0) {
+                a[x][y] = s;
+                s++;
+                x--;
+            }
+        }
+
         for (int i =0;i<n;i++){
-            System.out.println(Arrays.toString(a[i]));
+            for (int j =0; j<m;j++){
+                if (a[i][j] <10){
+                    System.out.print(a[i][j] + ",  ");
+                } else {
+                    System.out.print(a[i][j] + ", ");
+                }
+            }
+            System.out.println("");
         }
     }
 }
