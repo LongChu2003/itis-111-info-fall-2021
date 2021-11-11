9ad392a33bec1c52e5e5df3abe51dcf1b238abcb
LongChu2003 <long.chu@inbox.ru>
2021-11-11 12:41:49 +0300

    P3

--- homeworks/3/P3.java
+++ homeworks/3/P3.java
@@ -1,20 +1,18 @@
-import java.util.Scanner;
-public class P3{
-    public static void main(String[] args){
+public class P3 {
+    public static void main(String[] args) {
         Scanner vvod = new Scanner(System.in);
         int n = vvod.nextInt();
         boolean isProst = true;
-        for (int i = 2; i<n;i++)
-        {
-            if (n % i == 0)
-            {
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
                 isProst = false;
             }
         }
 
-        if (isProst == true && n !=1 && n !=0){
+        if (isProst == true && n != 1 && n != 0) {
             System.out.println("YES");
         } else {
             System.out.println("NO");
+        }
     }
 }
