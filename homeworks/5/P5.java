381081bd17970c495225ca32122bbc4fa4c25085
LongChu2003 <long.chu@inbox.ru>
2021-11-11 13:14:02 +0300

    P5

--- homeworks/5/P5.java
+++ homeworks/5/P5.java
@@ -7,27 +7,39 @@ public class P5{
         int max_2 = n;
         int sum_all =0;
         int sum = 0;
+        int x = 0;
+
         while (n !=0) {
             if (n > max_1) {
                 max_2 = max_1;
+                //System.out.println("max_2 = " + max_2 );
                 max_1 = n;
+                //System.out.println("max_1 = " + max_1);
                 sum = sum_all;
+
+
                 //System.out.println("if sum = " + sum);
                 sum_all = 0;
                 //System.out.println("if sum_all = " + sum_all);
             } else if (n > max_2) {
                 max_2 = n;
+                //System.out.println("max_2 = " + max_2 );
                 sum = sum_all;
                 //System.out.println("else if sum = " + sum);
                 sum_all = 0;
                 //System.out.println("else if sum_all = " + sum_all);
             } else {
                 sum_all+=n;
+                x++;
                 //System.out.println("else sum_all = " + sum_all);
             }
             n = vvod.nextInt();
         }
         //System.out.println(max_1 +" " + max_2);
-        System.out.println(sum);
+        if (x>0){
+            System.out.println(sum);
+        } else {
+            System.out.println("NO");
+        }
     }
 }
