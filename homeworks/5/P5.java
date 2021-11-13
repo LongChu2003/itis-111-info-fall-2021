b50ebbdebc12f6fc1032b11f0138a713f2913708
LongChu2003 <long.chu@inbox.ru>
2021-11-13 20:02:07 +0300

    P5

--- homeworks/5/P5.java
+++ homeworks/5/P5.java
@@ -3,27 +3,31 @@ public class P5{
     public static void main(String[] args){
         Scanner vvod = new Scanner(System.in);
         int n = vvod.nextInt();
-        int max1 = -11111;
-        int max2 = 0;
+        int max1 = n;
+        int max2 = -1111;
         int sum =0;
         int sum_final = 0;
         boolean f = true;
         while (n!=0){
+            sum +=n;
             if (n > max1){
                 max1 = n;
                 sum = 0;
+                f = true;
             } else if (n == max1 && f) {
                 max2 = max1;
-                sum_final = sum - max2 ;
+                sum_final = sum - Math.abs(max2) ;
+                //System.out.println("sum_final =" + sum_final );
                 f = false;
             }
-            sum +=n;
+            //sum +=n;
+            //System.out.println("sum = "+ sum);
             n = vvod.nextInt();
         }
         if (sum_final == 0){
             System.out.println("NO");
         } else {
-        System.out.println(sum_final);
+            System.out.println(sum_final);
         }
     }
 }
