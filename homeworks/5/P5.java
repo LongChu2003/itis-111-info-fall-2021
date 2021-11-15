4234ba812e0d74001e3ecb3cf16ab708fb66de7e
LongChu2003 <long.chu@inbox.ru>
2021-11-16 00:14:28 +0300

    P5

--- homeworks/5/P5.java
+++ homeworks/5/P5.java
@@ -7,24 +7,29 @@ public class P5{
         int max2 = 0;
         int sum =0;
         int sum_final = 0;
+        int x = 0;
         boolean f = true;
         while (n!=0){
-            sum +=n;
             if (n > max1){
                 max1 = n;
                 sum = 0;
+                sum_final = 0;
+
                 f = true;
             } else if (n == max1 && f) {
                 max2 = max1;
-                sum_final = sum - Math.abs(max2) ;
+                sum_final = sum ;
                 //System.out.println("sum_final =" + sum_final );
+                x++;
                 f = false;
+            } else {
+                sum +=n;
             }
             //sum +=n;
             //System.out.println("sum = "+ sum);
             n = vvod.nextInt();
         }
-        if (sum_final == 0){
+        if (x<1){
             System.out.println("NO");
         } else {
             System.out.println(sum_final);
