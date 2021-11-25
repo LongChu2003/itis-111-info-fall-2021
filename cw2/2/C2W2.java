fd1fd3c34d9e55ca42e10edd7808d5008bc6a0c2
LongChu2003 <long.chu@inbox.ru>
2021-11-25 10:56:33 +0300

    C2W2

--- cw2/2/C2W2.java
+++ cw2/2/C2W2.java
@@ -4,7 +4,13 @@ import java.util.*;
 public class C2W2 {
     // Решение задачи 2 следует писать здесь
     public static int solve2(int[] a) {
-      return 0;
+        int sum = 0;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] % 5 ==0){
+                sum += a[i];
+            }
+        }
+        return sum;
     }
     public static void main(String[] args) {
       System.out.println(solve2(new int[]{1,2,3,4,5}));
