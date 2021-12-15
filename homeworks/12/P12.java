d54629f49ba06e79520e0ce3fd7d09814a6ba5fe
LongChu2003 <long.chu@inbox.ru>
2021-12-15 22:00:02 +0300

    P12

--- homeworks/12/P12.java
+++ homeworks/12/P12.java
@@ -1,46 +1,50 @@
 import  java.util.*;
 public class P12 {
-    public static int[] carry(int q, int[] a){
-        int div = 0;
-        int l = a.length;
 
-        for (int i = 0; i < a.length; i++) {
-            a[i] += div;
-            div = a[i] / q;
-            a[i] = a[i] % q;
+        public static int[] extend(int[] a, int n) {
+                int[] b = new int[n];
+                for (int i = 0; i < Math.min(a.length, n); i++) {
+                        b[i] = a[i];
+                }
+                return b;
         }
 
-        if (div>0){
-            int[] b = new int[l+1];
-            for (int i=0; i<a.length; i++) {
-                b[i] = a[i];
-                a = b;
-            }
-            a[l] = div;
-            l++;
+        public static int[] carry(int q, int[] a) {
+                int c = 0;
+                int n = a.length;
+                for (int i = 0; i < n; i++) {
+                        a[i] += c;
+                        c = a[i] / q;
+                        a[i] %= q; // a[i] = a[i] % q
+                        if (a[i] < 0) {
+                                a[i] += q;
+                                c = -1;
+                        }
+                }
+                if (c > 0) {
+                        a = extend(a, n + 1);
+                        a[n] = c;
+                        n++;
+                }
+                while (n > 0 && a[n - 1] == 0) {
+                        n--;
+                }
+                a = extend(a, n);
+                return a;
         }
-        while (l>0 && a[l-1]==0) {
-            l--;
-        }
-        int[] b = new int[l];
-        for (int i=0; i<a.length; i++) {
-            b[i] = a[i];
-        }
-        a = b;
-        return a;
-    }
 
-    public static int[]  multiplyDigit(int q , int[] a, int k){
-        int n = a.length;
-        int[] b = new int[n];
-        for (int i=0; i<n; i++) {
-            b[i] = a[i] * k;
-        }
-        b = carry(q, b);
-        return b;
-    }
 
-    public static void main(String[] args){
-        System.out.println(Arrays.toString(multiplyDigit(10, new int[] {6,5,4,3,2,1}, 3)));
-    }
+        public static int[] multiplyDigit(int q, int[] a, int k) {
+                int n = a.length;
+                int[] c = new int[n];
+                for (int i=0; i<n; i++) {
+                        c[i] = a[i] * k;
+                }
+                c = carry(q, c);
+                return c;
+        }
+
+        public static void main(String[] args) {
+                System.out.println(Arrays.toString(multiplyDigit(100,new int[]{2,1}, 0)));
+        }
 }
