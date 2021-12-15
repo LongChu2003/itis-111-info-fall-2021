2fb048fd19d59fc15661f4b061bd960c25a6454b
LongChu2003 <long.chu@inbox.ru>
2021-12-15 22:37:14 +0300

    P12

--- homeworks/12/P12.java
+++ homeworks/12/P12.java
@@ -1,17 +1,18 @@
-import  java.util.*;
+import java.util.Arrays;
+
 public class P12 {
-
         public static int[] multiplyDigit(int q, int[] a, int k) {
-                int n = a.length;
-                int[] c = new int[n];
-                for (int i=0; i<n; i++) {
-                        c[i] = a[i] * k;
+                a = P11.carry(q, a);
+                int[] s = new int[a.length];
+                int c = 0;
+                for (int i = 0; i <= a.length - 1; i++) {
+                        s[i] += (a[i] * k + c) % q;
+                        c = (a[i] * k + c) / q;
                 }
-                c = P11.carry(q, c);
-                return c;
+                return s;
         }
-
         public static void main(String[] args) {
-                System.out.println(Arrays.toString(multiplyDigit(100,new int[]{2,1}, 0)));
+                int[] s = multiplyDigit(100,new int[]{1,2}, 0);
+                System.out.println(Arrays.toString(s));
         }
 }
